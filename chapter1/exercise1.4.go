7a7ce4245258e30b98e2dbe3cc6444aef314ce41
zhangying <zhangying@infosec.com.cn>
2021-03-18 18:08:43 +0800

    chapter1

--- chapter1/exercise1.4.go
+++ chapter1/exercise1.4.go
@@ -3,22 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	files := os.Args[1:]
+	// files := os.Args[1:]
+	files := []string{"echo1.go", "echo2.go", "echo3.go"}
 	if len(files) == 0 {
 		fmt.Println("need parameters")
 		return
 	}
 
-	counts := make(map[string]int)
+	counts := make(map[string][]string)
 	for _, fileName := range files {
 		content, err := os.ReadFile(fileName)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-
+		for _, line := range strings.Split(string(content), "\n") {
+			if counts[line] == nil {
+				counts[line] = make([]string, 1, 10)
+			}
+			counts[line] = append(counts[line], fileName)
+		}
 	}
+
+	for line, fileNames := range counts {
+		if len(fileNames) > 1 {
+			fmt.Printf("%s 'in' files %v\n", line, fileNames)
+		}
+	}
+	fmt.Println(counts)
 }
