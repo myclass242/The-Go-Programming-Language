b29d9eefe2697354900451167b1df8bcdac35ce4
yingzhang <1037236503@qq.com>
2021-03-21 22:01:22 +0800

    chapter2

--- chapter1/exercise1.12.go
+++ chapter1/exercise1.12.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"http"
 	"image"
 	"image/color"
 	"image/gif"
+	"log"
 	"math"
 	"math/rand"
 	"net/http"
@@ -28,8 +28,10 @@ func lissagus2(out http.ResponseWriter, in *http.Request) {
 		nframes = 64
 		delay   = 8
 	)
-	if index := strings.Index(in.URL.path, "?cycles="); index != -1 {
-		cycles, err = strconv.Atoi(in.URL.path[(index + len("?cycles")):])
+	if index := strings.Index(in.URL.Path, "?cycles="); index != -1 {
+		var err error
+		// cycles, err = strconv.Atoi(in.URL.Path)
+		cycles, err = strconv.Atoi(in.URL.Path[(index + len("?cycles")):])
 		if err != nil {
 			fmt.Printf("strconv.Atoi: %v\b", err)
 		}
@@ -41,14 +43,19 @@ func lissagus2(out http.ResponseWriter, in *http.Request) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles*2)*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5), blackIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
 	gif.EncodeAll(out, &anim)
+}
+
+func main() {
+	http.HandleFunc("/", lissagus2)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
